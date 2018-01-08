087e6d764b1635abd44494e84b0a43ee4c46a1af
Firas Chaaben <firaschaabencss@gmail.com>
2018-01-08 20:01:08 +0100

    run first test

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,5 @@
-var url = localStorage.getItem('music');
+//var url = localStorage.getItem('music');
+var url = "https://www.youtube.com/watch?v=3yyDul6FBDM&t=144s";
 function youtube_parser(url){
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
     var match = url.match(regExp);
@@ -6,5 +7,15 @@ function youtube_parser(url){
 }
 
 var id = youtube_parser(url);
-document.getElementById("player").src = "https://www.youtube.com/embed/" + id + "?autoplay=1" ;
+//document.getElementById("player").src = "https://www.youtube.com/embed/" + id + "?autoplay=1" ;
  
+
+function run(){
+document.getElementById("but") 
+var iframe = document.createElement('iframe');
+iframe.width="300" ;
+iframe.height="290" ;
+iframe.src = "https://www.youtube.com/embed/" + id + "?autoplay=1" ;
+document.body.appendChild(iframe);
+document.getElementById("but").type = "hidden";
+}
