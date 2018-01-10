fabe0a260fee152d797c7e58a6e47039e18e9502
Firas Chaaben <firaschaabencss@gmail.com>
2018-01-10 22:03:47 +0100

    fix setup

--- playlist.js
+++ playlist.js
@@ -1,17 +1,17 @@
-alert('oki');
 
+if (document.domain === "www.youtube.com"){
 var url = document.URL ;
 
 var id = document.title;
 
 
 if (localStorage.getItem('playlist') === null){
-	var lists = {id : url };
+	var lists = {[id] : url };
 	localStorage.setItem('playlist',JSON.stringify(lists));
 }
 
-alert("okis");
 
+}
 
 
 
