58f86af2eeffaf4c803d7c869de34d52b43ce0e9
Firas Chaaben <firaschaabencss@gmail.com>
2018-01-10 22:35:45 +0100

    fix param list vue pass

--- popup/popup.js
+++ popup/popup.js
@@ -9,15 +9,15 @@ function youtube_parser(url){
 var id = youtube_parser(url);
  
 
-function creation(id){
+function creation(name,link){
 var item = document.createElement('a');
 var n = document.createElement('br');
 var c = 'w3-button';
 item.classList.add(c);
-item.href = url  ;
+item.href = link  ;
 item.id = id  ;
 item.target = "_blank" ;
-item.innerHTML = "name" ;
+item.innerHTML = name ;
 document.body.appendChild(item);
 document.body.appendChild(n);
 }
@@ -26,14 +26,14 @@ function listsVues(){
 	/*
 	for (var key in lists) {
     if (lists.hasOwnProperty(key)) {
-        creation(lists[key]);
+        creation(key,lists[key]);
     }
 }
 */
 
 var i = 0 ;
 while(i<10){
-creation('but');
+creation('but',url);
 i++;
 }
 
